3b3b1fdeb73594e3f28c2cbc53e766312d1f38d3
alexclemente1985 <alexclemente1985@gmail.com>
2021-06-14 16:40:39 -0300

    Primeiro Commit do treino

---
+++ src/main/java/br/com/springboot/alexandre/treino/TreinoApplication.java
@@ -0,0 +1,14 @@
+package br.com.springboot.alexandre.treino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinoApplication.class, args);
+		System.out.println("Vai funcionar!!!");
+	}
+
+}
---
+++ src/test/java/br/com/springboot/alexandre/treino/TreinoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springboot.alexandre.treino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
