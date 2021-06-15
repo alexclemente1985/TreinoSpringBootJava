e9f682ea27e5026b7e0f759436750df6129d9623
alexclemente1985 <alexclemente1985@gmail.com>
2021-06-15 17:24:57 -0300

    Criação do OrderStatus Enum

--- src/main/java/br/com/springboot/alexandre/treino/entities/Order.java
+++ src/main/java/br/com/springboot/alexandre/treino/entities/Order.java
@@ -1,5 +1,6 @@
 package br.com.springboot.alexandre.treino.entities;
 
+import br.com.springboot.alexandre.treino.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -22,6 +23,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
 //Vai mostrar lista de pedidos do cliente
     //@JsonIgnore
     @ManyToOne
@@ -32,9 +35,10 @@ public class Order implements Serializable {
 
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -60,6 +64,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
