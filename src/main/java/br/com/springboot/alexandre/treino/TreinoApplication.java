5988a86cdecdf971efa3e4e04127e86028134201
alexclemente1985 <alexclemente1985@gmail.com>
2021-06-14 21:24:02 -0300

    Criação da base de dados H2

--- src/main/java/br/com/springboot/alexandre/treino/TreinoApplication.java
+++ src/main/java/br/com/springboot/alexandre/treino/TreinoApplication.java
@@ -2,6 +2,7 @@ package br.com.springboot.alexandre.treino;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class TreinoApplication {
