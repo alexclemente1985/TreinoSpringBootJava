5480fc29a9ef9cc9e5123346992ef5cfa36762ed
Alexandre Pinheiro <alexclemente1985@gmail.com>
2021-06-23 14:40:13 -0300

    Criação de Exceções para método delete de User

--- src/main/java/br/com/springboot/alexandre/treino/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/springboot/alexandre/treino/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.springboot.alexandre.treino.resources.exceptions;
 
+import br.com.springboot.alexandre.treino.services.exceptions.DatabaseException;
 import br.com.springboot.alexandre.treino.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,4 +21,15 @@ public class ResourceExceptionHandler {
 
         return ResponseEntity.status(status).body(err);
     }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(),status.value(), error, e.getMessage(),request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+    }
 }
+
+
--- src/main/java/br/com/springboot/alexandre/treino/services/UserService.java
+++ src/main/java/br/com/springboot/alexandre/treino/services/UserService.java
@@ -2,8 +2,11 @@ package br.com.springboot.alexandre.treino.services;
 
 import br.com.springboot.alexandre.treino.entities.User;
 import br.com.springboot.alexandre.treino.repositories.UserRepository;
+import br.com.springboot.alexandre.treino.services.exceptions.DatabaseException;
 import br.com.springboot.alexandre.treino.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try{
+            repository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
